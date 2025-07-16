3eeb417decdd8c76556a4ad4047dd121a7e92336
David <davidesparzaj22@gmail.com>
2025-07-16 16:40:41 -0500

    feat(src/server.ts):
    mount projectRouter at "/projects"

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { projectRouter } from "./projects/Adapters/project.router";
 export const server = express();
 server.use(express.json());
+
+server.use("/projects", projectRouter)
 
