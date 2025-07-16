b8815ba5de2300f1b7cb9f8ddead91c0010ee9ef
David <davidesparzaj22@gmail.com>
2025-07-16 16:24:39 -0500

    feat(src/projects/UseCases/project.services.ts):
    add listProjects in project.services.ts

---
+++ src/projects/UseCases/project.services.ts
@@ -0,0 +1,12 @@
+import { DifficultyLevel, LiveStatus } from "../../shared/types";
+import { ProjectModel } from "../Adapters/project.model";
+import { IProject } from "../Domain/project.domain";
+
+export const listProjects = async (): Promise<IProject[]> => {
+  const projects = await ProjectModel.find().lean().exec();
+  return Object.values(projects).map((project) => ({
+    ...project,
+    difficultyLevel: project.difficultyLevel as DifficultyLevel,
+    liveStatus: project.liveStatus as LiveStatus,
+  }));
+};
