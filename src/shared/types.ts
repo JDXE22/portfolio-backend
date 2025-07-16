55c7240e0ef99db3297fd019f312384ca283283b
David <davidesparzaj22@gmail.com>
2025-07-16 14:36:15 -0500

    feat(src/shared/types.ts):
    add LiveStatus and DifficultyLevel enums

---
+++ src/shared/types.ts
@@ -0,0 +1,12 @@
+export enum LiveStatus {
+    LIVE="Live",
+    DOWN="Down",
+    MAINTENANCE="Maintenance",
+    DEVELOPMENT="Development",
+}
+
+export enum DifficultyLevel {
+    EASY="Easy",
+    MEDIUM="Medium",
+    HARD="Hard",
+}
