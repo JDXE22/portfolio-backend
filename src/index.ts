698daaab7a8feb9980f5f949f81a03c3597795b4
David <davidesparzaj22@gmail.com>
2025-07-16 13:35:57 -0500

    fix(src/index.ts):
    handle startup errors by disconnecting DB
    
    and exiting

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,17 @@ import { PORT } from "./shared/config.env";
 import mongoose from "mongoose";
 import { MONGO_URI } from "./shared/config.env";
 
-async function startServer() {
+async function startServer(): Promise<void> {
   if (!MONGO_URI) {
     throw new Error("MONGO_URI is not defined.");
   }
   try {
     await mongoose.connect(MONGO_URI);
     server.listen(PORT);
-    console.log(`Server is running on port ${PORT}`);
   } catch (error) {
     console.error("Error starting the server:", error);
+    await mongoose.disconnect();
+    process.exit(1);
   }
 }
 
