bc934e191c2171c439f0af8e7218dc6b3b586c15
David <davidesparzaj22@gmail.com>
2025-07-16 14:15:20 -0500

    refactor(src/index.ts):
    replace inline mongoose connection with
    
    connectToDatabase helper

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
+import { connectToDatabase } from "./db";
 import { server } from "./server";
 import { PORT } from "./shared/config.env";
 import mongoose from "mongoose";
-import { MONGO_URI } from "./shared/config.env";
 
 async function startServer(): Promise<void> {
-  if (!MONGO_URI) {
-    throw new Error("MONGO_URI is not defined.");
-  }
+
   try {
-    await mongoose.connect(MONGO_URI);
+    await connectToDatabase();
     server.listen(PORT);
+    console.log(`Server is running on port ${PORT}`);
   } catch (error) {
     console.error("Error starting the server:", error);
     await mongoose.disconnect();
