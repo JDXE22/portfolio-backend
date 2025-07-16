27f19269836f83856385bfb828e59a6b01898574
David <davidesparzaj22@gmail.com>
2025-07-16 13:24:45 -0500

    feat(src/index.ts):
    connect to MongoDB before starting and add startup
    
    error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import { server } from "./server";
 import { PORT } from "./shared/config.env";
+import mongoose from "mongoose";
+import { MONGO_URI } from "./shared/config.env";
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  if (!MONGO_URI) {
+    throw new Error("MONGO_URI is not defined.");
+  }
+  try {
+    await mongoose.connect(MONGO_URI);
+    server.listen(PORT);
+    console.log(`Server is running on port ${PORT}`);
+  } catch (error) {
+    console.error("Error starting the server:", error);
+  }
+}
+
+startServer();
