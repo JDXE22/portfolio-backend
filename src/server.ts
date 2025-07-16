0a1be82b52f96e490a8fd19c4d930d8917304d0d
David <davidesparzaj22@gmail.com>
2025-07-16 17:44:29 -0500

    feat(src/server.ts):
    integrate centralized errorHandler middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { projectRouter } from "./projects/Adapters/project.router";
+import { errorHandler } from "./shared/error.handler";
 export const server = express();
 server.use(express.json());
+
+server.use(errorHandler)
 
 server.use("/projects", projectRouter)
 
