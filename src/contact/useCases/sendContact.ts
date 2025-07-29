5b69ac7adc878fd9299dde0efda826dacf1d914e
David <davidesparzaj22@gmail.com>
2025-07-29 15:28:46 -0500

    feat(src/contact/useCases/sendContact.ts):
    add attachment support in sendContact

--- src/contact/useCases/sendContact.ts
+++ src/contact/useCases/sendContact.ts
@@ -3,11 +3,18 @@ import { ContactMessage } from "../domain/contactMessage";
 import { GMAIL_USER } from "../../shared/config.env";
 import { NextFunction } from "express";
 
+
 export async function sendContact(
   data: ContactMessage,
   next: NextFunction
 ): Promise<void> {
-  const { name, email, subject, message, cc, bcc } = data;
+  const mailAttachments =data.attachments?.map((attachment) => ({
+    __filename: attachment.originalname,
+    content: attachment.buffer,
+    contentType: attachment.mimetype,
+  }))
+
+  const { name, email, subject, message, cc, bcc, attachments } = data;
   const html = `
         <h1>New contact from ${name}</h1>
         <p><strong>Email:</strong> ${email}</p>
@@ -15,6 +22,7 @@ export async function sendContact(
         <p><strong>Message:</strong> ${message}</p>
         ${cc ? `<p><strong>CC:</strong> ${cc}</p>` : ""}
         ${bcc ? `<p><strong>BCC:</strong> ${bcc}</p>` : ""}
+
     `;
 
   try {
@@ -23,6 +31,7 @@ export async function sendContact(
         to: GMAIL_USER,
         subject: `Portfolio Contact: ${subject}`,
         html,
+        attachments: mailAttachments,
       });
   } catch (error) {
     next(error);
