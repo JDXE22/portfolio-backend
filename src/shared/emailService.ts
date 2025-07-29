138f6533569346fa55d36daa17f8a471ea111efc
David <davidesparzaj22@gmail.com>
2025-07-28 20:23:00 -0500

    feat(src/shared/emailService.ts):
    configure EmailService transporter with Gmail SMTP

--- src/shared/emailService.ts
+++ src/shared/emailService.ts
@@ -1,5 +1,11 @@
 import nodemailer from 'nodemailer';
+import { GMAIL_HOST, GMAIL_PASS, GMAIL_PORT, GMAIL_USER } from './config.env';
 
-export const EmailService = {
-    transporter: nodemailer.createTransport({})
-}
+export const EmailService: nodemailer.Transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: GMAIL_USER,
+            pass: GMAIL_PASS
+        },
+        secure: false,
+    })
