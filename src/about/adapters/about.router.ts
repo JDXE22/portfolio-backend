8e91f5665c4c9988d69956dbd2a6a991f140cb4f
David <davidesparzaj22@gmail.com>
2025-07-31 15:41:12 -0500

    feat(src/about/adapters/about.router.ts):
    add aboutRouter and getAbout use case

---
+++ src/about/adapters/about.router.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { getAbout } from "../useCases/getAbout";
+
+export const aboutRouter = Router();
+
+aboutRouter.get("/", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const aboutInfo = await getAbout();
+        res.status(200).json(aboutInfo);
+    } catch (error) {
+        next(error);
+    }
+})
