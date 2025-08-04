f6a0fae6014078e5723fafcccaff2b9ceb9f8505
David <davidesparzaj22@gmail.com>
2025-08-04 15:50:46 -0500

    refactor(src/about/useCases/getAbout.ts):
    simplify socialLinks mapping in getAbout use case

--- src/about/useCases/getAbout.ts
+++ src/about/useCases/getAbout.ts
@@ -1,13 +1,12 @@
 import { AboutInfo } from "../domain/aboutInfo";
 import aboutData from "../about.json";
-import { SocialLink } from "../../shared/types";
 export async function getAbout(): Promise<AboutInfo> {
   return {
     ...aboutData,
     socialLinks: aboutData.socialLinks.map((link) => ({
       name: link.name,
-      url: link.url || "",
-      username: link.username || "",
-    })) as SocialLink[],
+      url: link.url,
+      username: link.username,
+    })),
   };
 }
