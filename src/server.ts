a287d5b6702f7bb75e858bc48615c35ce00222b7
David <davidesparzaj22@gmail.com>
2025-08-29 19:12:52 -0500

    fix(src/server.ts):
    update router import paths to relative paths for
    
    compatibility with CommonJS

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { projectRouter } from "@/projects/adapters/project.router";
+import { projectRouter } from "./projects/adapters/project.router";
 import { errorHandler } from "@/shared/error.handler";
 import { contactRouter } from "@/contact/adapters/contact.router";
 import { aboutRouter } from "@/about/adapters/about.router";
