2f8d6e67739fcb2d4e8245ce9472471c234dc564
JDEX22 <davidesparzaj22@gmail.com>
2025-10-02 15:47:23 -0500

    refactor(src/stack/useCases/getStack.ts):
    update getStack function to map and normalize
    
    stack data

--- src/stack/useCases/getStack.ts
+++ src/stack/useCases/getStack.ts
@@ -1,3 +1,17 @@
-import { Tech } from "../domain/stackInterface";
+import { CloudinaryAdapter } from '@/shared/adapters/cloudinary.config';
+import { Tech } from '@/stack/domain/stackInterface';
+import stack from '@/stack/stack.json';
+import { StackCategory } from '@/shared/types';
 
-export function getStack(): Tech[] {}
+export function getStack(): Tech[] {
+  const stackData = stack.map((tech) => ({
+    ...tech,
+    category: tech.category as StackCategory,
+    iconPublicId: CloudinaryAdapter.url(tech.iconPublicId, {
+      width: 64,
+      height: 64,
+      crop: 'fill',
+    }),
+  }));
+  return stackData;
+}
